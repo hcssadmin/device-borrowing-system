5f6fe9c4c700b0690ac3ae7439b5607e670f8ca7
hcssadmin <hcssadmin@web.hcs.edu.hk>
2026-04-29 15:32:42 +0800

    Update layout.js

--- js/layout.js
+++ js/layout.js
@@ -1,5 +1,5 @@
 async function loadNavbar() {
-  const res = await fetch('/navbar.html')
+  const res = await fetch('/navbar-admin.html')
   const html = await res.text()
   document.getElementById('navbar').innerHTML = html
 }
